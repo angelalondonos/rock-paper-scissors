50c4c65ac2f42b8e9625f2779f0e6dc6bd3debd5
Angela Londoño <l.angela05@gmail.com>
2022-07-28 23:37:23 -0500

    Add conditional while for 3 equal attempts

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 //juego de piedra, papel y tijera donde el usuario ingresa una opción y la máquina responde para ver si coinciden o no
 
-let player;
-let pc = numberRandom(1,3);
+let player=0;
+let pc=0;
+let wins=0;
+let losses=0;
 
+/* Create number randon for pc */
 function numberRandom(min,max){
     return Math.floor(Math.random()* (max - min + 1)+min);
 }
 
-function play(player){
-    if(player == 1 && pc == 3){
-        console.log("Ganaste!!")
-    }else if(player == 2 && pc == 1){
-        console.log("Ganaste!!")
-    }else  if(player == 3 && pc == 2){
-        console.log("Ganaste!!")
-    }else  if(player == pc){
-        console.log("Empate!!")
-    }else {
-        console.log("Sigue intentando")
+function play(){
+
+    while(wins < 3 && losses<3){
+        player = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
+        pc = numberRandom(1,3);
+        if(player == 1 && pc == 3 || player == 2 && pc == 1 || player == 3 && pc == 2){
+            console.log("Ganaste!!");
+            wins =wins +1; 
+        }else  if(player == pc){
+            console.log("Empate!!");
+        }else {
+            console.log("Perdiste");
+            losses = losses +1;
+        }
     }
+    console.log("Wins: " + wins + "   Losses: " + losses);
+
+
+
+
+
+
+    
 }
